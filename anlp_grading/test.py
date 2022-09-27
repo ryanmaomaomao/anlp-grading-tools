90bb951f8589d181c77723d56c452a4c943a6624
ryanmaomaomao <42869424+ryanmaomaomao@users.noreply.github.com>
2022-09-27 16:21:07 -0400

    adaptive-fintuning

--- anlp_grading/test.py
+++ anlp_grading/test.py
@@ -8,41 +8,41 @@ student_folder = Path('/mnt/code')
 scores_folder = Path('/mnt/scores')
 
 
+# execute_cli_timeout(
+#     'cd /mnt/code && '
+#     'python3 /mnt/code/classifier.py '
+#     '--option=pretrain '
+#     '--epochs=10 '
+#     '--lr=1e-3 '
+#     '--use_gpu '
+#     '--batch_size=64 '
+#     '--train=/mnt/data/sst-train.txt '
+#     '--dev=/mnt/data/sst-dev.txt '
+#     '--test=/mnt/data/sst-test.txt '
+#     '--dev_out=/mnt/scores/sst-dev-output.txt '
+#     '--test_out=/mnt/scores/sst-test-output.txt ',
+#     timeout=4500
+# )
+
+# sst_pretrain_dev_acc = compare_outputs(std="/mnt/data/sst-dev.txt", result="/mnt/scores/sst-dev-output.txt")
+# sst_pretrain_test_acc = compare_outputs(std="/mnt/data/sst-test.txt", result="/mnt/scores/sst-test-output.txt")
+
+# print('sst pretrain dev accuracy ',sst_pretrain_dev_acc)
+# print('sst pretrain test accuracy ', sst_pretrain_test_acc)
+
 execute_cli_timeout(
     'cd /mnt/code && '
     'python3 /mnt/code/classifier.py '
-    '--option pretrain '
-    '--epochs 10 '
-    '--lr 1e-3 '
+    '--option=adaptive-finetune '
+    '--epochs=10 '
+    '--lr=1e-5 '
     '--use_gpu '
-    '--batch_size 64 '
-    '--train /mnt/data/sst-train.txt '
-    '--dev /mnt/data/sst-dev.txt '
-    '--test /mnt/data/sst-test.txt '
-    '--dev_out /mnt/scores/sst-dev-output.txt '
-    '--test_out /mnt/scores/sst-test-output.txt ',
-    timeout=4500
-)
-
-sst_pretrain_dev_acc = compare_outputs(std="/mnt/data/sst-dev.txt", result="/mnt/scores/sst-dev-output.txt")
-sst_pretrain_test_acc = compare_outputs(std="/mnt/data/sst-test.txt", result="/mnt/scores/sst-test-output.txt")
-
-print('sst pretrain dev accuracy ',sst_pretrain_dev_acc)
-print('sst pretrain test accuracy ', sst_pretrain_test_acc)
-
-execute_cli_timeout(
-    'cd /mnt/code && '
-    'python3 /mnt/code/classifier.py '
-    '--option finetune '
-    '--epochs 10 '
-    '--lr 1e-5 '
-    '--use_gpu '
-    '--batch_size 64 '
-    '--train /mnt/data/sst-train.txt '
-    '--dev /mnt/data/sst-dev.txt '
-    '--test /mnt/data/sst-test.txt '
-    '--dev_out /mnt/scores/sst-dev-output.txt '
-    '--test_out /mnt/scores/sst-test-output.txt ',
+    '--batch_size=8 '
+    '--train=/mnt/data/sst-train.txt '
+    '--dev=/mnt/data/sst-dev.txt '
+    '--test=/mnt/data/sst-test.txt '
+    '--dev_out=/mnt/scores/sst-dev-output.txt '
+    '--test_out=/mnt/scores/sst-test-output.txt ',
     timeout=8100
 )
 
@@ -57,16 +57,16 @@ print('sst finetune test accuracy ', sst_finetune_test_acc)
 execute_cli_timeout(
     'cd /mnt/code && '
     'python3 /mnt/code/classifier.py '
-    '--option finetune '
-    '--epochs 10 '
-    '--lr 1e-5 '
+    '--option=adaptive-finetune '
+    '--epochs=10 '
+    '--lr=1e-5 '
     '--use_gpu '
-    '--batch_size 8 '
-    '--train /mnt/data/cfimdb-train.txt '
-    '--dev /mnt/data/cfimdb-dev.txt '
-    '--test /mnt/data/cfimdb-test.txt '
-    '--dev /mnt/data/cfimdb-dev.txt  '
-    '--dev_out /mnt/scores/cfimdb-dev-output.txt ',
+    '--batch_size=8 '
+    '--train=/mnt/data/cfimdb-train.txt '
+    '--dev=/mnt/data/cfimdb-dev.txt '
+    '--test=/mnt/data/cfimdb-test.txt '
+    '--dev=/mnt/data/cfimdb-dev.txt  '
+    '--dev_out=/mnt/scores/cfimdb-dev-output.txt ',
     timeout=12600
 )
 
@@ -75,8 +75,11 @@ cfimdb_finetune_dev_acc = compare_outputs(std="/mnt/data/cfimdb-dev.txt", result
 print('cfimdb finetune dev accuracy ',cfimdb_finetune_dev_acc)
 
 
+
+
 with open(scores_folder / os.environ["ANDREW_ID"], "w") as f:
-    f.write(f"{sst_pretrain_dev_acc}\t{sst_pretrain_test_acc}\t{sst_finetune_dev_acc}\t{sst_finetune_test_acc}\t{cfimdb_finetune_dev_acc}")
+#     f.write(f"{sst_pretrain_dev_acc}\t{sst_pretrain_test_acc}\t{sst_finetune_dev_acc}\t{sst_finetune_test_acc}\t{cfimdb_finetune_dev_acc}")
+    f.write(f"{sst_finetune_dev_acc}\t{sst_finetune_test_acc}\t{cfimdb_finetune_dev_acc}")
 
 execute_cli_timeout(
     'rm -rf /mnt/code/*',
